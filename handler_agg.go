7500bc2df883ae8b817dcee28dafe05d8488b36d
Rodabaugh <erik@erikrodabaugh.com>
2024-10-07 22:03:42 -0700

    Aggregate feeds, and browse posts

--- handler_agg.go
+++ handler_agg.go
@@ -2,15 +2,80 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/Rodabaugh/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	ctx := context.Background()
-	feed, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.arguments) != 1 {
+		return fmt.Errorf("usage: %s timeBetweenRequests (e.g. 30s, 1m, 1h)", cmd.name)
 	}
-	fmt.Printf("%+v\n", feed)
+	time_between_reqs := cmd.arguments[0]
+
+	timeBetweenRequests, err := time.ParseDuration(time_between_reqs)
+	if err != nil {
+		return fmt.Errorf("unable to parse %s as a time duration\n%w", time_between_reqs, err)
+	}
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get next feed to fetch: %w", err)
+	}
+	currentTime := time.Now()
+	_, err = s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{ID: nextFeed.ID, UpdatedAt: currentTime})
+	if err != nil {
+		return fmt.Errorf("couldn't mark feed as fetched: %w", err)
+	}
+
+	feed, err := fetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("was unable to fetch feed: %w", err)
+	}
+
+	for _, item := range feed.Channel.Item {
+		publishedAt := sql.NullTime{}
+		if t, err := time.Parse(time.RFC1123Z, item.PubDate); err == nil {
+			publishedAt = sql.NullTime{
+				Time:  t,
+				Valid: true,
+			}
+		}
+
+		_, err = s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			FeedID:    nextFeed.ID,
+			Title:     item.Title,
+			Description: sql.NullString{
+				String: item.Description,
+				Valid:  true,
+			},
+			Url:         item.Link,
+			PublishedAt: publishedAt,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+				continue
+			}
+			log.Printf("Couldn't create post: %v", err)
+			continue
+		}
+	}
+	log.Printf("Collected feed %s. %v posts found", nextFeed.Name, len(feed.Channel.Item))
+
 	return nil
 }
--- main.go
+++ main.go
@@ -64,6 +64,8 @@ func main() {
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollows))
 	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
+	cmds.register("browse", middlewareLoggedIn(handlerBrowse))
+	cmds.register("help", handlerHelp)
 
 	// Look up and run the handler
 	if err := cmds.run(appState, cmd); err != nil {
@@ -79,4 +81,21 @@ func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) 
 		}
 		return handler(s, cmd, user)
 	}
+}
+
+func handlerHelp(s *state, cmd command) error {
+	fmt.Println("Available Commands")
+	fmt.Println("* login")
+	fmt.Println("* register")
+	fmt.Println("* reset")
+	fmt.Println("* users")
+	fmt.Println("* agg")
+	fmt.Println("* addfeed")
+	fmt.Println("* feeds")
+	fmt.Println("* follow")
+	fmt.Println("* following")
+	fmt.Println("* unfollow")
+	fmt.Println("* browse")
+	fmt.Println("* help")
+	return nil
 }
